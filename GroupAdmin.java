330a5e0d97464f84834b4d490909c939814536f8
Nitay <nitaylevi1998@gmail.com>
2022-12-15 09:54:11 +0200

    GA upd

--- GroupAdmin.java
+++ GroupAdmin.java
@@ -1,13 +1,26 @@
 import java.util.ArrayList;
 
+/** The GroupAdmin class contains within it the database of states (UdoableStringBuilder)
+ * and a database of clients (members) who receive updates on any changes made to the database.
+ *@author Naor Tzadok & Nitai Levy*/
 public class GroupAdmin implements Sender{
-    private ArrayList<Member> members = new ArrayList<Member>();
+    private ArrayList<Member> members = new ArrayList<>();
     private UndoableStringBuilder usbState = new UndoableStringBuilder();
+
+    /** This function registers an object that implements the Member interface into
+     * our database of clients.
+     * @param obj -> The object that will be added to our group.
+     */
     @Override
     public void register(Member obj) {
         members.add(obj);
     }
 
+    /** This function unregisters an object from
+     * our database of clients. This object won't get updates when we change the
+     * group state (UdoableStringBuilder) and its current state (UdoableStringBuilder) will change to null.
+     * @param obj -> The object that will be omitted from our group.
+     */
     @Override
     public void unregister(Member obj) {
         int pos = members.indexOf(obj);
@@ -16,6 +29,11 @@ public class GroupAdmin implements Sender{
 
     }
 
+    /** This function inserts a string into our database of states (UdoableStringBuilder)
+     * and updates each client.
+     * @param offset - The offset.
+     * @param obj - A string.
+     */
     @Override
     public void insert(int offset, String obj) {
         usbState.insert(offset, obj);

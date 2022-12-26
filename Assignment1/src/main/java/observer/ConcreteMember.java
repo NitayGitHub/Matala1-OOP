dcc62795e73075f31139f43f8a719a9ca0be9b7a
naor554 <117055432+naor554@users.noreply.github.com>
2022-12-27 00:14:33 +0200

    Add files via upload

---
+++ Assignment1/src/main/java/observer/ConcreteMember.java
@@ -0,0 +1,26 @@
+package Matala_1;
+
+
+/** The ConcreteMember (observer) class contains within it a shallow copy (memState) of the UndoableStringBuilder database from the GroupAdmin.
+ *@author Naor Tzadok & Nitai Levy*/
+
+public class ConcreteMember implements Member{
+	private UndoableStringBuilder memState = null;
+
+	/** Updates the class string sequence to point to the given UndoableStringBuilder parameter.
+	 * If the ConcreteMember was added to a GroupAdmin Class database of clients, the function updates
+	 * the memState to point to that GroupAdmin database of states. If it was removed, the function updates
+	 * the memState to be null.
+	 *
+	 * @param usb -> The GroupAdmin UndoableStringBuilder
+	 */
+	@Override
+	public void update(UndoableStringBuilder usb) {
+			memState = usb;
+		}
+
+	public UndoableStringBuilder getSB() {
+		return memState;
+	}
+
+}
---
+++ Assignment1/src/main/java/observer/GroupAdmin.java
@@ -0,0 +1,95 @@
+package Matala_1;
+
+import java.util.ArrayList;
+
+/** The GroupAdmin (observable) class contains within it the database of states (UndoableStringBuilder)
+ * and a database of clients (members) who receive updates on any changes made to the database.
+ *@author Naor Tzadok & Nitai Levy*/
+public class GroupAdmin implements Sender{
+    private final ArrayList<Member> members = new ArrayList<>();
+    private final UndoableStringBuilder usbState = new UndoableStringBuilder();
+    public int size(){
+        return members.size();
+    }
+
+    public UndoableStringBuilder getUsbState(){
+        return usbState;
+    }
+
+    /** This function registers an object that implements the Member interface into
+     * our database of clients. After that, the object updates its UndoableStringBuilder
+     * to point to usbState.
+     * @param obj -> The object that will be added to our group.
+     */
+
+    @Override
+    public void register(Member obj) {
+        members.add(obj);
+
+    }
+
+    /** This function unregisters an object from
+     * our database of clients. This object won't get updates when we change the
+     * group state (UndoableStringBuilder) and its current state (UndoableStringBuilder) will change to null.
+     * @param obj -> The object that will be omitted from our group.
+     */
+    @Override
+    public void unregister(Member obj) {
+        int pos = members.indexOf(obj);
+        members.get(pos).update(null);
+        members.remove(obj);
+
+    }
+
+    /** This function inserts a string into our database of states (UndoableStringBuilder).
+     * @param offset - The offset.
+     * @param obj - A string.
+     */
+    @Override
+    public void insert(int offset, String obj) {
+        usbState.insert(offset, obj);
+        for(Member m : members){
+            m.update(usbState);
+        }
+
+    }
+
+    /** This function appends the specified string to our database of states (UndoableStringBuilder).
+     * @param obj - A string.
+     */
+    @Override
+    public void append(String obj) {
+        usbState.append(obj);
+        for(Member m : members){
+            m.update(usbState);
+        }
+
+    }
+
+    /** This function removes the characters in a substring of our database of states. The substring begins
+     * at the specified start and extends to the character at index
+     * end - 1 or to the end of the sequence if no such character exists.
+     * If start is equal to end, no changes are made.
+     * @param start – The beginning index, inclusive.
+     * @param end – The ending index, exclusive.
+     */
+    @Override
+    public void delete(int start, int end) {
+        usbState.delete(start, end);
+        for(Member m : members){
+            m.update(usbState);
+        }
+
+    }
+    /** This function reverts our database of states to the state before the current one.*/
+
+    @Override
+    public void undo() {
+        usbState.undo();
+        for(Member m : members){
+            m.update(usbState);
+        }
+
+    }
+
+}
